7da07599faed1b6a306d8ab56d30d8262c7444b6
JGaston26 <125668861+JGaston26@users.noreply.github.com>
2023-10-02 09:33:38 -0400

    Add files via upload

--- src/Menu.java
+++ src/Menu.java
@@ -8,6 +8,7 @@ public class Menu {
 
     public String printMenu()
     {
+        // making menu
         System.out.println("Krabby Patty…………...1.25         Krabby Meal…………….. 3.50");
         System.out.println(" 	w/sea cheese…. 1.50			Double Krabby Meal…… 3.75");
         System.out.println("Double Krabby Patty….. 2.00     Triple Krabby Meal…...... 4.00");
@@ -26,55 +27,55 @@ public class Menu {
         return null;
     }
 
-    public double giveCost(double cost,String item)
+    public double giveCost(String item)
     {
-        cost = 0.00;
-        if(item.equals("Krabby Patty"))
+        //making it so the cost of item is added based on what's typed
+        double cost = 0.00;
+        if (item.equals("Krabby Patty"))
         {
-            cost += 1.25;
-            System.out.println(cost);
+            cost = 1.25;
         }else if(item.equals("Krabby Patty w/sea cheese")){
-            cost += 1.50;
+            cost = 1.50;
         } else if (item.equals("Double Krabby Patty")) {
-            cost += 2.00;
+            cost = 2.00;
         } else if (item.equals("Double Krabby Patty w/sea cheese")) {
-            cost += 2.25;
+            cost = 2.25;
         }else if(item.equals("Tripple Krabby Patty")){
-            cost += 3.00;
+            cost = 3.00;
         }else if (item.equals("Tripple Krabby Patty w/sea cheese")) {
-            cost += 3.25;
+            cost = 3.25;
         } else if (item.equals("Krabby Meal")) {
-            cost += 3.50;
+            cost = 3.50;
         } else if (item.equals("Double Krabby Meal")) {
-            cost += 3.75;
+            cost = 3.75;
         } else if (item.equals("Triple Krabby Meal")) {
-            cost += 4.00;
+            cost = 4.00;
         } else if (item.equals("Salty Sea Dog")) {
-            cost += 1.25;
+            cost = 1.25;
         } else if (item.equals("Footlong")) {
-            cost += 2.00;
+            cost = 2.00;
         } else if (item.equals("Golden Loaf")) {
-            cost += 2.00;
+            cost = 2.00;
         } else if (item.equals("Golden Loaf w/sauce")) {
-            cost += 2.50;
+            cost = 2.50;
         } else if (item.equals("Small Coral Bits")) {
-            cost += 1.00;
+            cost = 1.00;
         }else if (item.equals("Medium Coral Bits")) {
-            cost += 2.00;
+            cost = 2.00;
         }else if (item.equals("Large Coral Bits")) {
-            cost += 3.00;
+            cost = 3.00;
         } else if (item.equals("Small Seafoam Soda")) {
-            cost += 1.00;
+            cost = 1.00;
         }else if (item.equals("Medium Seafoam Soda")) {
-            cost += 2.00;
+            cost = 2.00;
         }else if (item.equals("Large Seafoam Soda")) {
-            cost += 3.00;
+            cost = 3.00;
         }else if (item.equals("Kelp Shake")) {
-            cost += 2.00;
+            cost = 2.00;
         } else if (item.equals("Kelp Rings")) {
-            cost += 1.50;
+            cost = 1.50;
         } else if (item.equals("Kelp Rings w/ salty sauce")) {
-            cost += 2.00;
+            cost = 2.00;
         }
         return cost;
     }
--- src/Unit1Project.java
+++ src/Unit1Project.java
@@ -2,51 +2,59 @@ import java.util.Scanner;
 public class Unit1Project {
     public static void main(String[]args)
     {
+        //creating the scanner and menu object
         Scanner scan = new Scanner(System.in);
         Menu menu = new Menu();
 
+        // Welcome to the Krusty Krab
         System.out.println("Welcome to the Krusty Krab, home of the Krabby Patty!");
+        System.out.println(" ");
+        System.out.println(" ");
         System.out.print("How many people are in your group: ");
         int numPpl = scan.nextInt();
-
+        scan.nextLine();
 
         System.out.print("What is the tip percentage: ");
         int tipPercent = scan.nextInt();
-        double price = 0;
-        String cost = "";
+        scan.nextLine();
+        String item = "";
         double totalPrice = 0.0;
-        while (!cost.equals("End"))
+        while (!item.equals("End"))
         {
             menu.printMenu();
             System.out.print("Enter item on menu(type End to end): ");
-            cost = scan.nextLine();
-            switch (cost) {
-                case "Krabby Patty" -> {
-                    price += 1.25;
-                    System.out.println(cost);
-                }
-                case "Krabby Patty w/sea cheese", "Kelp Rings" -> price += 1.50;
-                case "Double Krabby Patty", "Golden Loaf", "Footlong", "Medium Coral Bits", "Medium Seafoam Soda", "Kelp Shake" -> price += 2.00;
-                case "Double Krabby Patty w/sea cheese" -> price += 2.25;
-                case "Triple Krabby Patty", "Large Coral Bits", "Large Seafoam Soda" -> price += 3.00;
-                case "Triple Krabby Patty w/sea cheese" -> price += 3.25;
-                case "Krabby Meal" -> price += 3.50;
-                case "Double Krabby Meal" -> price += 3.75;
-                case "Triple Krabby Meal" -> price += 4.00;
-                case "Salty Sea Dog" -> price += 1.25;
-                case "Golden Loaf w/sauce" -> price += 2.50;
-                case "Small Coral Bits", "Small Seafoam Soda" -> price += 1.00;
-                case "Kelp Rings w/ salty sauce" -> cost += 2.00;
-            }
-            totalPrice += price;
+            item = scan.nextLine();
+            // old code that doesn't work
+
+//            price = menu.giveCost(item);
+//            switch (cost) {
+//                case "Krabby Patty" -> {
+//                    price += 1.25;
+//                    System.out.println(cost);
+//                }
+//                case "Krabby Patty w/sea cheese", "Kelp Rings" -> price += 1.50;
+//                case "Double Krabby Patty", "Golden Loaf", "Footlong", "Medium Coral Bits", "Medium Seafoam Soda", "Kelp Shake" -> price += 2.00;
+//                case "Double Krabby Patty w/sea cheese" -> price += 2.25;
+//                case "Triple Krabby Patty", "Large Coral Bits", "Large Seafoam Soda" -> price += 3.00;
+//                case "Triple Krabby Patty w/sea cheese" -> price += 3.25;
+//                case "Krabby Meal" -> price += 3.50;
+//                case "Double Krabby Meal" -> price += 3.75;
+//                case "Triple Krabby Meal" -> price += 4.00;
+//                case "Salty Sea Dog" -> price += 1.25;
+//                case "Golden Loaf w/sauce" -> price += 2.50;
+//                case "Small Coral Bits", "Small Seafoam Soda" -> price += 1.00;
+//                case "Kelp Rings w/ salty sauce" -> cost += 2.00;
+//            }
+            totalPrice += menu.giveCost(item);;
             System.out.println(totalPrice);
 
         }
-        totalPrice = totalPrice/2;
+        // prints out bill info
         System.out.println("-----------------------");
         System.out.println("Total bill before tip: $" + String.format("%.2f",totalPrice));
         System.out.println("Total percentage: " + tipPercent + "%");
 
+        // calculating the valuess
         double realPercent = (double) tipPercent/100;
         double tipAmount = totalPrice * realPercent;
         double totalAndTip = totalPrice + tipAmount;
@@ -54,6 +62,7 @@ public class Unit1Project {
         double tipPerPerson = tipAmount/numPpl;
         double perPerson = totalAndTip/numPpl;
 
+        //her comes the sun, i mean here comes the string formatting do do do do
         System.out.println("Total tip: $" + String.format("%.2f", tipAmount));
         System.out.println("Total bill with tip: $" + String.format("%.2f", totalAndTip));
         System.out.println("Per person cost before tip: $" + String.format("%.2f", perPersonNoTip));
